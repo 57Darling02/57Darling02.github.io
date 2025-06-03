89d396a301ea4fcd7ebec61832a0c6f8e54eb0ff
ZYJiang <3144223912@qq.com>
2025-06-03 16:47:15 +0800

    update

--- site_config.ts
+++ site_config.ts
@@ -1,70 +1,98 @@
 export default {
-  // VitePress 站点基本配置,必填，允许留空
-  site_name: "My Awesome Site",
-  site_description: "这是一个使用 VitePress 构建的文档站点。",
-  site_url: "/",
-  author: '57Darling02',
-  defaultFocusMode: false, // 是否默认开启焦点模式
-  isDark: null, // 是否默认开启深色模式, null 则会跟随系统
-  // 首页配置
-  home: {
-    mainTitle: "My Awesome Site",
-    subTitles: ['世界上只有一种英雄主义', '那就是在认清生活的真相后', '依然热爱生活'],//打字机效果的副标题，使用字符串列表
-  },
-  background: '/wallpaper/1.webp',
-  pageSize: 8, //首页文章列表分页大小，默认为8
-  sortedMethor : 'lastUpdated' as "title"|"date"|"lastUpdated", //排序方式，默认为lastUpdated，可选值为lastUpdated、date、title
-
-  // 最后更新时间相关选项
+    // VitePress 站点基本配置,必填，允许留空
+    site_name: "57Darling02's Blog",
+    site_description: "哈哈哈哈呵呵呵呵",
+    site_url: "/",
+    author: '57Darling02',
+    defaultFocusMode: false, // 是否默认开启焦点模式
+    isDark: null, // 是否默认开启深色模式, null 则会跟随系统
+    // 首页配置
+    home: {
+        mainTitle: "57Darling02's Blog",
+        subTitles: ['世界上只有一种英雄主义', '那就是在认清生活的真相后', '依然热爱生活', '如果你抑郁了，说明你活在过去', '如果你焦虑了，说明你活在未来', '当你平静了，你才活在当下。'],//打字机效果的副标题，使用字符串列表
+    },
+    background: '/wallpaper/1.webp',
+    pageSize: 8, //首页文章列表分页大小，默认为8
+    sortedMethor : 'date' as "title"|"date"|"lastUpdated", //排序方式，默认为lastUpdated，可选值为lastUpdated、date、title
+    
+    // 最后更新时间相关选项
   lastUpdated: {
     use: true, // 是否开启最后更新时间
     text: '📆最后更新于', // 最后更新时间的文本
   },
-
-  // 侧边简介卡
-  avatar: "https://resource-un4.pages.dev/article/yjtp.webp", //头像地址
-  name: '57Darling02',
-  position: '全栈开发、优化算法爱好者',
-  bio: '红红火火恍恍惚惚',
-  socialLinks: [
-    {
-      name: 'GitHub',
-      icon: 'fa-brands fa-github',
-      url: 'https://github.com/57Darling02/'
-    }
-  ],
-  footer: {
-    message: 'Released under the MIT License.',
-    copyright: 'Copyright © 2025-present My Awesome Site'
-  },
-  //菜单栏
-  menuToc: true, //是否显示文章目录
-  menuItems: [
-    {
-      label: '好用的网站',
-      icon: 'fa-solid fa-browser',
-      children: [
+    // 侧边简介卡
+    avatar: "https://resource-un4.pages.dev/article/yjtp.webp",
+    name: '57Darling02',
+    position: '全栈开发、优化算法爱好者',
+    bio: '红红火火恍恍惚惚',
+    socialLinks: [
         {
-          key: 'action1',
-          label: '课程平台青春版',
-          icon: 'fa-solid fa-browser',
-          link: 'https://course.bjtu.top'
+            name: 'GitHub',
+            icon: 'fa-brands fa-github',
+            url: 'https://github.com/57Darling02/'
         },
-        
-      ]
+    ],
+    footer: {
+        message: 'Released under the MIT License.',
+        copyright: "Copyright © 2025-present 57Darling02's Blog"
     },
-    {
-      label: '更多',
-      icon: '',
-      children: [
+  
+    //菜单栏
+    menuToc: true, //是否显示文章目录
+    menuItems: [
         {
-          key: 'action1',
-          label: '标签',
-          icon: 'fa-solid fa-tags',
-          link: '/Tags/'
+            label: '网站',
+            icon: 'fa-solid fa-browser',
+            children: [
+                {
+                    key: 'action1',
+                    label: '课程平台青春版',
+                    icon: 'fa-solid fa-browser',
+                    link: 'https://course.bjtu.top'
+                },
+                {
+                    key: 'action2',
+                    label: 'alist云盘',
+                    icon: 'fa-solid fa-cloud',
+                    link: 'https://alist.57d02.cn'
+                },
+                {
+                    key: 'action3',
+                    label: 'processon',
+                    icon: 'fa-solid fa-browser',
+                    link: 'https://www.processon.com/'
+                },
+                {
+                    key: 'action3',
+                    label: 'ORC公式识别',
+                    icon: 'fa-solid fa-browser',
+                    link: 'http://www.codeinword.com/'
+                },
+                {
+                    key: 'action3',
+                    label: 'word中加入代码块',
+                    icon: 'fa-solid fa-browser',
+                    link: 'http://www.codeinword.com/'
+                },
+                {
+                    key: 'action3',
+                    label: 'deepseek',
+                    icon: 'fa-solid fa-browser',
+                    link: 'https://chat.deepseek.com/'
+                },
+            ]
         },
-      ]
-    },
-    
-  ],
+        {
+            label: '更多',
+            icon: '',
+            children: [
+                {
+                    key: 'action1',
+                    label: '标签',
+                    icon: 'fa-solid fa-tags',
+                    link: '/Tags/'
+                },
+            ]
+        },
+    ],
 }
